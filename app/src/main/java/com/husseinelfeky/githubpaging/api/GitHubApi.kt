15445b498b2388daddfbe9334f0e6c12ffda1200
Hussein El Feky <hussein.feky@gmail.com>
2020-05-03 04:20:38 +0200

    Add OkHttp interceptors

--- app/src/main/java/com/husseinelfeky/githubpaging/api/GitHubApi.kt
+++ app/src/main/java/com/husseinelfeky/githubpaging/api/GitHubApi.kt
@@ -4,27 +4,20 @@ import com.husseinelfeky.githubpaging.persistence.entities.GitHubRepo
 import com.husseinelfeky.githubpaging.persistence.entities.User
 import io.reactivex.Single
 import retrofit2.http.GET
-import retrofit2.http.Headers
 import retrofit2.http.Path
 import retrofit2.http.Query
 
-const val token = " 75eaba855c297ee5498a3bf699b95460ad78b924"
-
 interface GitHubApi {
 
-    @Headers("Authorization: token$token")
     @GET("users")
     suspend fun getUsers(): List<User>
 
-    @Headers("Authorization: token$token")
     @GET("users/{user_name}/repos")
     suspend fun getRepositories(@Path("user_name") userName: String): List<GitHubRepo>
 
-    @Headers("Authorization: token$token")
     @GET("users")
     fun getUsersRx(@Query("page") page: Int, @Query("per_page") perPage: Int = 10): Single<List<User>>
 
-    @Headers("Authorization: token$token")
     @GET("users/{user_name}/repos")
     fun getGitHubReposRx(
         @Path("user_name") userName: String,
@@ -32,7 +25,6 @@ interface GitHubApi {
         @Query("per_page") perPage: Int = 10
     ): Single<List<GitHubRepo>>
 
-    @Headers("Authorization: token$token")
     @GET("users/{user_name}/repos")
     fun getRepositoriesRx(@Path("user_name") userName: String): Single<List<GitHubRepo>>
 }
--- app/src/main/java/com/husseinelfeky/githubpaging/api/RetrofitClient.kt
+++ app/src/main/java/com/husseinelfeky/githubpaging/api/RetrofitClient.kt
@@ -1,9 +1,14 @@
 package com.husseinelfeky.githubpaging.api
 
+import com.husseinelfeky.githubpaging.api.adapters.GitHubRepoJsonAdapter
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
-import hu.akarnokd.rxjava3.retrofit.RxJava3CallAdapterFactory
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.Response
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.converter.scalars.ScalarsConverterFactory
 
@@ -15,16 +20,36 @@ object RetrofitClient {
 
     fun getClient(): Retrofit {
         if (!::retrofit.isInitialized) {
+            val okHttpClient = OkHttpClient.Builder()
+                .addInterceptor(loggingInterceptor())
+                .addInterceptor { tokenInterceptor(it) }
+                .build()
+
             val moshi = Moshi.Builder()
                 .add(KotlinJsonAdapterFactory())
+                .add(GitHubRepoJsonAdapter())
                 .build()
+
             retrofit = Retrofit.Builder()
                 .baseUrl(BASE_URL)
+                .client(okHttpClient)
                 .addConverterFactory(ScalarsConverterFactory.create())
                 .addConverterFactory(MoshiConverterFactory.create(moshi))
-                .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
                 .build()
         }
         return retrofit
+    }
+
+    private fun loggingInterceptor() = HttpLoggingInterceptor().apply {
+        setLevel(HttpLoggingInterceptor.Level.BODY)
+    }
+
+    private fun tokenInterceptor(chain: Interceptor.Chain): Response = chain.run {
+        proceed(
+            request().newBuilder()
+                .addHeader("Authorization", "token 75eaba855c297ee5498a3bf699b95460ad78b924")
+                .build()
+        )
     }
 }
