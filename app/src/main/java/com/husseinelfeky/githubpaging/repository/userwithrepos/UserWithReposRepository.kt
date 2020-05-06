125192c63a0dda797dd2d2cac3246bf4b647a89f
Hussein El Feky <hussein.feky@gmail.com>
2020-05-06 06:53:36 +0200

    Return a flowable on success of fetching users and all repos together

--- app/src/main/java/com/husseinelfeky/githubpaging/repository/userwithrepos/UserWithReposRepository.kt
+++ app/src/main/java/com/husseinelfeky/githubpaging/repository/userwithrepos/UserWithReposRepository.kt
@@ -3,6 +3,7 @@ package com.husseinelfeky.githubpaging.repository.userwithrepos
 import com.husseinelfeky.githubpaging.common.paging.caching.FetchingStrategy
 import com.husseinelfeky.githubpaging.persistence.entities.UserWithRepos
 import io.reactivex.Flowable
+import io.reactivex.Observable
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
 import timber.log.Timber
@@ -18,24 +19,24 @@ class UserWithReposRepository(
     ): Flowable<List<UserWithRepos>> {
         return usersFetchingRepo.fetchItems(page)
             .doOnSuccess {
-                Timber.i("Fetched ${it.size} user repos")
+                Timber.i("Fetched ${it.size} users")
             }
             .doOnError {
                 Timber.e(it)
             }
             .toObservable()
-            .flatMapIterable { users ->
-                users.map { user ->
-                    gitHubReposFetchingRepo.fetchItems(fetchingStrategy, user.userName, user.id)
-                        .doOnSuccess {
-                            Timber.i("Fetched ${it.size} users")
-                        }
-                        .doOnError {
-                            Timber.e(it)
-                        }
-                        .subscribeOn(Schedulers.io())
-                        .subscribe()
-                }
+            .flatMap { users -> Observable.fromIterable(users) }
+            .flatMap { user ->
+                gitHubReposFetchingRepo.fetchItems(fetchingStrategy, user.userName, user.id)
+                    .doOnSuccess {
+                        Timber.i("Fetched ${it.size} repos of user id ${user.id}")
+                    }
+                    .doOnError {
+                        Timber.e(it)
+                    }
+                    .subscribeOn(Schedulers.io())
+                    .observeOn(Schedulers.io())
+                    .toObservable()
             }
             .doOnComplete {
                 Timber.i("Fetching completed")
